8179fa6c733cc22507edf93bee4663738e3d3be5
Cristian Baldi <bld.cris.96@gmail.com>
2016-10-09 17:15:23 +0200

    Start to fake mathoid endpoints

--- app.py
+++ app.py
@@ -1,32 +1,59 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from urllib import parse
 
 import uuid
 import subprocess
+import latex2mathml.converter
 
 app = Flask("Latex 2 Svg")
 
-@app.route("/")
+
+@app.route("/", methods=["POST"])
 def makeIndex():
-    return render_template("html/index.html")
+    return render_json()
 
-@app.route("/render/<formula>")
-def renderGET(formula):
-    formula = parse.unquote(formula)
-    return renderFormula(formula)
+@app.route("/texvcinfo", methods=["POST"])
+def texvcinfo():
+    input_query = request.form['q']
+    response = { "success": True,
+            "checked": input_query,
+            "requiredPackages":[],
+            "identifiers": [""],
+            "endsWithDot": False
+        }
+    return jsonify(response)
+        
+@app.route("/json", methods=["POST"])
+def render_json():
+    input_query = request.form['q']
+    response = {
+        "speakText": "bla bla bla",
+        "svg": renderSVG(input_query),
+        "mml": latex2mathml.converter.convert(input_query),
+        "success": True,
+        "log": "success",
+        "sanetex": input_query
+    }
+    return jsonify(response)
 
-@app.route("/renderPOST", methods=['POST'])
-def renderPOST():
-    return renderFormula(request.json['formula'])
+@app.route("/mml", methods=["POST"])
+def render_mml():
+    input_query = request.form['q']
+    return latex2mathml.converter.convert(input_query)
 
-def renderFormula(formula):
+@app.route("/svg", methods=["POST"])
+def render_svg():
+    input_query = request.form['q']
+    return renderSVG(input_query)
+
+def renderSVG(formula):
     filename = str(uuid.uuid4());
     filenameTEX = filename + ".tex"
     filenamePDF = filename + ".pdf"
     filenameSVG = filename + ".svg"
 
     formula = formula.strip()
-    
+
     latex = render_template("latex/document.tex", formula=formula)
     with open("/tmp/" + filename + ".tex", "w") as file:
         file.write(latex)
@@ -40,6 +67,5 @@ def renderFormula(formula):
     
     return s
 
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)    
+    app.run(host='0.0.0.0', port=10044, debug=True)    
--- runDebug.sh
+++ runDebug.sh
@@ -3,7 +3,8 @@
 mkdir -p /tmp/remakemathoid
 
 docker run --rm \
+    --name wtl-dev-remakemathoid \
     -v $(pwd):/latex2svg \
     -v /tmp/remakemathoid:/tmp \
-    -p 5000:5000 \
+    -p 10044:10044 \
     wikitolearn/remakemathoid
